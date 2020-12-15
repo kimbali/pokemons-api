2dcf3a8c9e80a3509a0a5b2d69825892be01383b
Kimbali <kimgarcianton@hotmail.com>
2020-12-15 14:57:28 +0100

    feat: :sparkles: Add language param to api
    Test test test as a way of life
    
    Only suported languages

--- logic/logic.js
+++ logic/logic.js
@@ -1,18 +1,22 @@
 const pokemons = require("../dataMoc")
-const { ENG } = require("../config")
+const languages = require("../config")
 
 const logic = {
-    createCatalog({ page, lang = ENG }) {
-        const size = 30;
+    createCatalog(params) {
+        if (!params) return [];
+        const { page, lang } = params;
+
+        if(!languages[lang]) throw new Error('Invalid language')
+
+        const size = 20;
         const startingAt = page * size;
 
         return Promise.resolve()
             .then(() => {
-                if(pokemons.length === 0) throw new LogicError ('There are no pokemons in town');
                 const chunk = [];
                 for (let i = startingAt; i < (startingAt + size); i++) {
                     const { id, name, type } = pokemons[i];
-                    const pokemon = { id, name: name[lang], type};
+                    const pokemon = { id, name: name[languages[lang]], type};
                     chunk.push(pokemon);
                 }
                 return chunk;
--- logic/logic.spec.js
+++ logic/logic.spec.js
@@ -7,24 +7,80 @@ describe('logic', () => {
 
     describe('Create a filtered catalog', () => {
 
-        it('should retrive pokemons list', () =>
-            logic.createCatalog()
+        true && it('should return an empty array without params', () => {
+            const result = logic.createCatalog();
+            expect(result).to.be.an('array');
+            expect(result).to.have.lengthOf(0);
+        })
+
+        true && it('should throw new error if language is not found', () => {
+            expect( function(){ logic.createCatalog({ page: 0, lang: 'Swahili'})})
+                .throw('Invalid language');
+        })
+
+        true && it('should retrive a list of 30 pokemons', () =>
+            logic.createCatalog({ page: 0, lang: 'GB'})
                 .then(pokemons => {
                     expect(pokemons).to.be.an('array');
+                    expect(pokemons).to.have.lengthOf(20);
                 })
         )
 
-        it('Each pokemon of the list should have only id, name and type', () =>
-            logic.createCatalog()
+        true && it('should retrive the first page', () =>
+            logic.createCatalog({ page: 0, lang: 'GB'})
                 .then(pokemons => {
-                    const singlePokemon = Object.keys(pokemons[0]);
+                    expect(pokemons[0].id).to.equal(1);
+                    expect(pokemons[pokemons.length - 1].id).to.equal(20);
+                })
+        )
+
+        true && it('should the second page', () =>
+            logic.createCatalog({ page: 1, lang: 'GB'})
+                .then(pokemons => {
+                    expect(pokemons[0].id).to.equal(21);
+                    expect(pokemons[pokemons.length - 1].id).to.equal(40);
+                })
+        )
+
+        true && it('Each pokemon of the list should have only id, name and type', () =>
+            logic.createCatalog({ page: 0, lang: 'GB'})
+                .then(pokemons => {
+                    const singlePokemonProp = Object.keys(pokemons[0]);
                     const expectedProperties = [ 'id', 'name', 'type']
 
-                    expect(singlePokemon).to.be.an('array');
-                    expect(singlePokemon).to.have.lengthOf(3);
-                    expect(singlePokemon[0]).to.equal(expectedProperties[0]);
-                    expect(singlePokemon[1]).to.equal(expectedProperties[1]);
-                    expect(singlePokemon[2]).to.equal(expectedProperties[2]);
+                    expect(singlePokemonProp).to.be.an('array');
+                    expect(singlePokemonProp).to.have.lengthOf(3);
+                    expect(singlePokemonProp[0]).to.equal(expectedProperties[0]);
+                    expect(singlePokemonProp[1]).to.equal(expectedProperties[1]);
+                    expect(singlePokemonProp[2]).to.equal(expectedProperties[2]);
+                })
+        )
+
+        true && it('should retrive a pokemons list with english language', () =>
+            logic.createCatalog({ page: 0, lang: 'GB'})
+                .then(pokemons => {
+                    expect(pokemons[0].name).to.equal('Bulbasaur')
+                })
+        )
+
+        true && it('should retrive a pokemons list with french language', () =>
+            logic.createCatalog({ page: 0, lang: 'FR'})
+                .then(pokemons => {
+                    expect(pokemons[0].name).to.equal('Bulbizarre')
+                })
+        )
+
+        true && it('should retrive a pokemons list with chinese language', () =>
+            logic.createCatalog({ page: 0, lang: 'CN'})
+                .then(pokemons => {
+                    expect(pokemons[0].name).to.equal('妙蛙种子')
+                })
+        )
+
+        true && it('should retrive a pokemons list with japanese language', () =>
+            logic.createCatalog({ page: 0, lang: 'JP'})
+                .then(pokemons => {
+                    expect(pokemons[0].name).to.equal('フシギダネ')
                 })
         )
     })
